9d69470bebaacf207632b45ee606a4716ce53289
Ollie Pugh <oliver.pugh@mac.com>
2022-11-26 01:00:02 +0000

    Changed the fairy light colours

--- src/utils/palette.js
+++ src/utils/palette.js
@@ -20,22 +20,14 @@ const colours = {
 };
 
 const fairyLightsColours = {
-  c1: "#ff4500",
-  c2: "#ffa800",
-  c3: "#ffd635",
-  c4: "#00cc78",
-  c5: "#7eed56",
-  c6: "#2450a4",
-  c7: "#3690ea",
-  c8: "#51e9f4",
-  c9: "#811e9f",
-  c10: "#b44ac0",
-  c11: "#ff99aa",
-  c12: "#9c6926",
-  c13: "#000000",
-  c14: "#898d90",
-  c15: "#d4d7d9",
-  c16: "#ffffff",
+  c1: "#ffffff",
+  c2: "#ff0000",
+  c3: "#ffa500",
+  c4: "#ffff00",
+  c5: "#00ff00",
+  c6: "#0000ff",
+  c7: "#ff00ff",
+  c8: "#000000",
 };
 
 const baubleColours = {
