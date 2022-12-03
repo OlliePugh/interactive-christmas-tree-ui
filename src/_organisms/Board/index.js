bb56422b2b90665a1960a047180e046e186e5417
Ollie Pugh <oliver.pugh@mac.com>
2022-12-03 12:39:56 +0000

    Added border around currently selected pixel

--- src/_organisms/Board/index.js
+++ src/_organisms/Board/index.js
@@ -7,6 +7,11 @@ import { baubleColours } from "../../utils/palette";
 import { writeData } from "../../utils/fb_funcs";
 import { functions } from "../../config/fb_config";
 
+const rgbToHex = (r, g, b) => {
+  if (r > 255 || g > 255 || b > 255) throw "Invalid color component";
+  return "#" + ((r << 16) | (g << 8) | b).toString(16);
+};
+
 const Board = ({
   width,
   height,
@@ -18,6 +23,43 @@ const Board = ({
   const [currentClickPos, setCurrentClickPos] = useState();
   const [loaded, setLoaded] = useState(false);
   const canvasRef = useRef();
+  const currentOpenPixel = useRef();
+
+  useEffect(() => {
+    if (currentClickPos) {
+      setCurrentOpenPixel(currentClickPos);
+    }
+  }, [currentClickPos]);
+
+  const setCurrentOpenPixel = useCallback(({ col, row }) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    // revert old pixel if it exists
+    if (currentOpenPixel.current) {
+      const oldPixel = context.getImageData(
+        currentOpenPixel.current.col * 10 + 5,
+        currentOpenPixel.current.row * 10 + 5,
+        1,
+        1
+      );
+
+      const oldColour = rgbToHex(
+        oldPixel.data[0],
+        oldPixel.data[1],
+        oldPixel.data[2]
+      );
+
+      setPixelColour(
+        currentOpenPixel.current.row,
+        currentOpenPixel.current.col,
+        oldColour
+      );
+    }
+
+    drawOutline(col, row);
+
+    currentOpenPixel.current = { col, row };
+  }, []);
 
   const setColour = async (colour) => {
     if (!placeCooldownCheck()) {
@@ -26,6 +68,7 @@ const Board = ({
     }
 
     const id = width * currentClickPos.row + currentClickPos.col;
+    currentOpenPixel.current = null;
     setPixelColour(currentClickPos.row, currentClickPos.col, colour);
     try {
       writeData(functions, {
@@ -41,8 +84,24 @@ const Board = ({
   const setPixelColour = (row, col, colour) => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+
     context.fillStyle = colour;
     context.fillRect(col * 10, row * 10, 10, 10);
+  };
+
+  const drawOutline = (col, row) => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    context.beginPath();
+    const scaledCol = col * 10;
+    const scaledRow = row * 10;
+    context.lineWidth = 1;
+    context.moveTo(scaledCol + 0.5, scaledRow + 0.5);
+    context.lineTo(scaledCol + 9.5, scaledRow + 0.5);
+    context.lineTo(scaledCol + 9.5, scaledRow + 9.5);
+    context.lineTo(scaledCol + 0.5, scaledRow + 9.5);
+    context.lineTo(scaledCol + 0.5, scaledRow + 0.5);
+    context.stroke();
   };
 
   const fillEntireBoard = useCallback(
