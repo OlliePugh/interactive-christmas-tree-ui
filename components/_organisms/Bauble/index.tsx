2351a92141ad752fabc51c3069767f88b4f5f0a1
Ollie Pugh <oliver.pugh@mac.com>
2023-12-07 21:59:07 +0000

    add bauble thumbnails and recalibrate lights

--- components/_organisms/Bauble/index.tsx
+++ components/_organisms/Bauble/index.tsx
@@ -1,5 +1,7 @@
+import { storage } from "@/config/fb_config";
 import { Box, SxProps } from "@mui/system";
-import Image from "next/image";
+import { getDownloadURL, ref } from "firebase/storage";
+import { useEffect, useState } from "react";
 
 interface BaubleProps {
   openBauble: (id: number) => void;
@@ -7,14 +9,23 @@ interface BaubleProps {
   sx: SxProps;
 }
 
-const Bauble = ({ openBauble, id, sx }: BaubleProps) => (
-  <Box
-    sx={{ ...sx, position: "absolute", zIndex: 10 }}
-    onClick={() => openBauble(id)}
-  >
-    {/* eslint-disable-next-line @next/next/no-img-element */}
-    <img height={80} width={80} alt="Bauble" src="/bauble.png" />
-  </Box>
-);
+const Bauble = ({ openBauble, id, sx }: BaubleProps) => {
+  const [imgUrl, setImgUrl] = useState("./bauble.png");
+
+  useEffect(() => {
+    const imageRef = ref(storage, `board${id}.jpeg`);
+    getDownloadURL(imageRef).then(setImgUrl);
+  }, [id]);
+
+  return (
+    <Box
+      sx={{ ...sx, position: "absolute", zIndex: 10 }}
+      onClick={() => openBauble(id)}
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img height={60} width={60} alt="Bauble" src={imgUrl} />
+    </Box>
+  );
+};
 
 export default Bauble;
